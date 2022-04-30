98ef15f27464a35eafc868297b947fd5ed58fc81
Melescope <kjkgz123@gmail.com>
2022-04-30 15:09:27 +0900

    내가 replace 메서드로 해봤는데 뭐가 잘 안되니??!

--- 30-72410/main.js
+++ 30-72410/main.js
@@ -1,20 +1,39 @@
 function solution(new_id) {
-    idlist = [];
-    for (i of new_id){
-        idlist.push(i);
-    }
-    for (i of idlist){
-        if (i === i.toUpperCase()) { //대==>소 변환
-            i = i.toLowerCase();
-        }
-        if ( (i != i.toLowerCase()) && (isNaN(i)) && ( !! '-' ) && (!! '_') && (!! '.') ) {
-            idlist.splice(i,1); // i 지우기
-        }
-        if ( (i === '.') && ()) ){
+    id = new_id
 
-        }
-        
+    id = id.toLowerCase();
+    
+    id.replace( /[a-z][0-9][-,_,.]/g , '' );
+
+    id.replace(/[.]{2,}/g, '.');
+
+    if (id[0] === '.') {
+        id.slice(0,1);
     }
-    var answer = '';
-    return answer;
-}
+    if (id[-1] === '.') {
+        id.slice(-1,1);
+    }
+    if (id === ''){
+        id = 'a';
+    }
+    if (id.length >= 16){
+        id.slice(15,-1);
+    }
+    if (id[-1] === '.') {
+        id.slice(-1,1);
+    }
+    if (id.length === 2 ) {
+        id += id[1];
+    }
+    if (id.length === 1 ) {
+        id += id[0];
+        id += id[0];
+    }
+    return id;
+    }
+    
+    
+
+
+console.log(solution("...!@BaT#*..y.abcdefghijklm"))
+console.log('The correct answer is !!! bat.y.abcdefghi !!! ')
