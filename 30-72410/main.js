d00edf728c65ac63bb9fdac4eda2bfd194a83b80
Melescope <kjkgz123@gmail.com>
2022-04-30 18:35:18 +0900

    왜 레인지 함수가 작동 안하냐...

--- 30-72410/main.js
+++ 30-72410/main.js
@@ -1,34 +1,52 @@
 function solution(new_id) {
     id = new_id
+    console.log('원본 ', id);
 
     id = id.toLowerCase();
-    
-    id.replace( /[a-z][0-9][-,_,.]/g , '' );
+    console.log('대문자 조지기' , id);
+////////////////////////////////////////////////
+    id.replace( /[a-z], [0-9],[-,_,.] /g , '' );
+    console.log('특수문자 조지기 ' , id);
 
-    id.replace(/[.]{2,}/g, '.');
+    id.replace(/.{2,}/g, '.');
+    console.log('... 조지기' , id);
 
     if (id[0] === '.') {
         id.slice(0,1);
     }
+    console.log('첫 . 없애기',id);
+
     if (id[-1] === '.') {
         id.slice(-1,1);
     }
+    console.log('마지막 . 없애기',id);
+
     if (id === ''){
         id = 'a';
     }
+    console.log('공백이면 a만 남기기' , id);
+
     if (id.length >= 16){
         id.slice(15,-1);
     }
+    console.log('16자 이상이면 짤라버리기',id);
+
     if (id[-1] === '.') {
         id.slice(-1,1);
     }
+    console.log('마짐ㄱ 글자가 . 이면 자르기',id);
+
     if (id.length === 2 ) {
         id += id[1];
     }
+    console.log('2글자라네!',id);
+
     if (id.length === 1 ) {
         id += id[0];
         id += id[0];
     }
+    console.log('1글자라네!' ,id);
+
     return id;
     }
     
@@ -37,3 +55,4 @@ function solution(new_id) {
 
 console.log(solution("...!@BaT#*..y.abcdefghijklm"))
 console.log('The correct answer is !!! bat.y.abcdefghi !!! ')
+console.log('abc'[length('abc')])
