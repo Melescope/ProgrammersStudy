af4922689d6b0408bb0fb42ef828db82b9d47050
Melescope <kjkgz123@gmail.com>
2022-05-03 14:47:32 +0900

    30-68644 구글링해서 풀었는데, sort 작동이 이해가 잘 안간다.

--- 30-72410/main.js
+++ 30-72410/main.js
@@ -11,11 +11,10 @@ function solution(new_id) {
 
   if (id[0] === ".") {
     id = id.replace(id[0], "");
-    //console.log("첫 . 삭제", id);
   }
 
   if (id[id.length - 1] === ".") {
-    id = id.slice(0, id.length - 2);
+    id = id.slice(0, id.length - 1);
     console.log("마지막 . 없애기", id);
   }
 
@@ -24,51 +23,11 @@ function solution(new_id) {
   }
 
   if (id.length >= 16) {
-    console.log(
-      "15글자로 줄이기 전",
-      id[0],
-      id[1],
-      id[2],
-      id[3],
-      id[4],
-      id[5],
-      id[6],
-      id[7],
-      id[8],
-      id[9],
-      id[10],
-      id[11],
-      id[12],
-      id[13],
-      id[14],
-      id[15]
-    );
-    id = id.slice(0, 14);
-    // 위에 이거 15글자만 남기는거 아님?? 왜 14글자만 남길까??
-    console.log(
-      "15글자로 줄이기",
-      id[0],
-      id[1],
-      id[2],
-      id[3],
-      id[4],
-      id[5],
-      id[6],
-      id[7],
-      id[8],
-      id[9],
-      id[10],
-      id[11],
-      id[12],
-      id[13],
-      id[14],
-      id[15]
-    );
+    id = id.slice(0, 15);
   }
 
   if (id[id.length - 1] === ".") {
-    id = id.slice(0, id.length - 2);
-    console.log("마지막 . 없애기2", id);
+    id = id.slice(0, id.length - 1);
   }
 
   if (id.length === 2) {
@@ -90,3 +49,4 @@ console.log(
 console.log("The correct answer is : bat.y.abcdefghi");
 console.log("The solution answer is : ", solution("abcdefghijklmn.p"));
 console.log("The correct answer is : abcdefghijklmn");
+// 풀었다! 2022 05 03 indexOf(,X) 부분에 탐색 시작 위치를 정하면 중복 탐색을 피할 수 있음.
