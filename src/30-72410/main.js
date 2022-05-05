b2e9646613bc7051704d948f80006f543d6905f2
Melescope <kjkgz123@gmail.com>
2022-05-05 23:53:29 +0900

    신규 ID 입력 문제 - main.test 작성

--- src/30-72410/main.js
+++ src/30-72410/main.js
@@ -15,7 +15,7 @@ function solution(new_id) {
 
   if (id[id.length - 1] === ".") {
     id = id.slice(0, id.length - 1);
-    console.log("마지막 . 없애기", id);
+    //console.log("마지막 . 없애기", id);
   }
 
   if (id === "") {
@@ -41,12 +41,6 @@ function solution(new_id) {
 
   return id;
 }
+module.exports = { solution };
 
-console.log(
-  "The solution answer is : ",
-  solution("...!@BaT#*..y.abcdefghijklm")
-);
-console.log("The correct answer is : bat.y.abcdefghi");
-console.log("The solution answer is : ", solution("abcdefghijklmn.p"));
-console.log("The correct answer is : abcdefghijklmn");
 // 풀었다! 2022 05 03 indexOf(,X) 부분에 탐색 시작 위치를 정하면 중복 탐색을 피할 수 있음.
